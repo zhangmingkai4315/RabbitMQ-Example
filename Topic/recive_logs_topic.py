e7d78d276328ed963a42bfc5e924a64762ec5d7e
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2015-08-17 15:12:17 +0800

    增加topic统配支持

---
+++ Topic/recive_logs_topic.py
@@ -0,0 +1,26 @@
+import pika
+import secret
+import sys
+
+connection=pika.BlockingConnection(pika.ConnectionParameters(host=secret.ipaddress))
+channel=connection.channel()
+
+channel.exchange_declare(exchange='logs_topic',type='topic')
+
+result=channel.queue_declare(exclusive=True)
+queue_name=result.method.queue
+severities=sys.argv[1:]
+if not severities:
+    print >> sys.stderr, 'Usage: %s [info] [error] [warning]' %(sys.argv[0])
+    sys.exit(1)
+
+for severity in severities:
+    channel.queue_bind(exchange='logs_topic',queue=queue_name,routing_key=severity)
+
+print "[*] Waitting for logs.To exits press CTRL+C"
+
+def callBack(ch,method,properties,body):
+    print "[x] %r:%r"%(method.routing_key,body)
+
+channel.basic_consume(callBack,queue=queue_name,no_ack=True)
+channel.start_consuming()
